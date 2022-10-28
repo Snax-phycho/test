0df51c545b67b27cbb1d82a9e38261c5151a54b6
Gabriel Biren <gbiren@google.com>
2022-10-28 18:54:04 +0000

    tulip: Use AIDL WiFi service
    Bug: 205044134
    Test: Presubmit tests
    Change-Id: Icdaa448e2ed59b638056593adb4bcd769f34220d
    Signed-off-by: GhostMaster69-dev <rathore6375@gmail.com>
    Signed-off-by: Sa-Sajjad <sqsa93103@gmail.com>

--- device.mk
+++ device.mk
@@ -451,7 +451,7 @@ PRODUCT_COPY_FILES += \
 
 # Wifi
 PRODUCT_PACKAGES += \
-    android.hardware.wifi@1.0-service \
+    android.hardware.wifi-service \
     hostapd \
     libwifi-hal-qcom \
     TetheringConfigOverlay \
