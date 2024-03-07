870b27ecc687890061a2376f5e2304d8ba465ae1
Adithya R <gh0strider.2k18.reborn@gmail.com>
2024-03-07 11:04:28 -0300

    lavender: rootdir: Tune cpusets with sunfish
    * https: //android.googlesource.com/device/google/sunfish/+/refs/heads/master/init.hardware.rc#454
    
    - Move background cpuset to CPU0-1
    Background tasks are not UX-critical.
    Free up cores to work on more important
    UX tasks.
    
    - Reduce number of CPUs for system-background apps
    system-background isn't too heavy load to handle where cpuset
    gives it 6 CPUs to rely on.
    Reduce it to 4 CPUs, effectively reducing CPU power load.
    
    Change-Id: Ibfdcdd42a7915853980540eb6ae3bec2244bb981
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Co-authored-by: Richard Raya <rdxzv.dev@gmail.com>
    Co-authored-by: chrisl7 <wandersonrodriguesf1@gmail.com>
    Signed-off-by: GhostMaster69-dev <rathore6375@gmail.com>
    Signed-off-by: Sã Śâjjãd <mdsojola12@gmail.com>

--- rootdir/bin/init.qcom.post_boot.sh
+++ rootdir/bin/init.qcom.post_boot.sh
@@ -2364,10 +2364,10 @@ case "$target" in
 
             # cpuset settings
             echo 0-7 > /dev/cpuset/top-app/cpus
-            echo 0-3,6-7 > /dev/cpuset/foreground/cpus
+            echo 0-5,7 > /dev/cpuset/foreground/cpus
             echo 0-1 > /dev/cpuset/background/cpus
             echo 0-3 > /dev/cpuset/system-background/cpus
-            echo 0-3 > /dev/cpuset/restricted/cpus
+            echo 2-5 > /dev/cpuset/restricted/cpus
 
             # disable thermal bcl hotplug to switch governor
             echo 0 > /sys/module/msm_thermal/core_control/enabled
