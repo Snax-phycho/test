b428bb9916201b176ef0a2039e4aadcfd60b740f
GhostMaster69-dev <rathore6375@gmail.com>
2024-05-29 07:28:10 +0530

    tulip: rootdir: Tune PRLMK
    Signed-off-by: GhostMaster69-dev <rathore6375@gmail.com>
    Signed-off-by: Sã Śâjjãd <mdsojola12@gmail.com>

--- rootdir/bin/init.qcom.post_boot.sh
+++ rootdir/bin/init.qcom.post_boot.sh
@@ -518,12 +518,11 @@ else
             ;;
           *)
             #Set PPR parameters for all other targets.
-            echo $set_almk_ppr_adj > /sys/module/process_reclaim/parameters/min_score_adj
-            echo 0 > /sys/module/process_reclaim/parameters/enable_process_reclaim
-            echo 50 > /sys/module/process_reclaim/parameters/pressure_min
-            echo 70 > /sys/module/process_reclaim/parameters/pressure_max
-            echo 30 > /sys/module/process_reclaim/parameters/swap_opt_eff
-            echo 512 > /sys/module/process_reclaim/parameters/per_swap_size
+            echo 150 > /sys/module/process_reclaim/parameters/min_score_adj
+            echo 20 > /sys/module/process_reclaim/parameters/pressure_min
+            echo 40 > /sys/module/process_reclaim/parameters/pressure_max
+            echo 20 > /sys/module/process_reclaim/parameters/swap_opt_eff
+            echo 4096 > /sys/module/process_reclaim/parameters/per_swap_size
             ;;
         esac
         echo 40 > /proc/sys/vm/vfs_cache_pressure
