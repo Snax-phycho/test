3793d6c264d0d7542daf5598740326aff68b409f
Sa-Sajjad <sqsa93103@gmail.com>
2024-06-28 10:27:12 +0600

    derp
    Signed-off-by: Sa-Sajjad <sqsa93103@gmail.com>

--- derp_lavender.mk
+++ derp_lavender.mk
@@ -8,19 +8,18 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common SuperiorOS stuff
-$(call inherit-product, vendor/superior/config/common.mk)
+# Inherit some common derpfestOS stuff
+$(call inherit-product, vendor/derp/config/common_full_phone.mk)
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_SUPPORTS_QUICK_TAP := true
 TARGET_FACE_UNLOCK_SUPPORTED := true
-SUPERIOR_OFFICIAL := true
 
 # Inherit from lavender device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 $(call inherit-product, $(LOCAL_PATH)/device-hidl.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := superior_lavender
+PRODUCT_NAME := derp_lavender
 PRODUCT_DEVICE := lavender
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi Note 7
