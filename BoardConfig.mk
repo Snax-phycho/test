4100c6cbb6b8c081dc43870d993b3ca875543a30
Sa-Sajjad <sqsa93103@gmail.com>
2024-07-01 17:47:37 +0600

    los test
    Signed-off-by: Sa-Sajjad <sqsa93103@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -99,7 +99,7 @@ DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix_legacy.xml \
     $(DEVICE_PATH)/device_framework_compatibility_matrix.xml \
-    vendor/derp/config/device_framework_matrix.xml
+    vendor/lineage/config/device_framework_matrix.xml
     
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
 DEVICE_MATRIX_FILE := hardware/qcom-caf/common/compatibility_matrix.xml
