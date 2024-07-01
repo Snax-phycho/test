b7b9a639e0b5d30d4a63b8af8aabbd38051a810d
Sa-Sajjad <sqsa93103@gmail.com>
2024-07-01 09:30:19 +0600

    fix conflict
    Signed-off-by: Sa-Sajjad <sqsa93103@gmail.com>

--- derp_lavender.mk
+++ derp_lavender.mk
@@ -13,6 +13,7 @@ $(call inherit-product, vendor/derp/config/common_full_phone.mk)
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_SUPPORTS_QUICK_TAP := true
 TARGET_FACE_UNLOCK_SUPPORTED := true
+WITH_GMS := false
 
 # Inherit from lavender device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
