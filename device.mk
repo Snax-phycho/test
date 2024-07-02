6b7fb12a25f2239011296ada5fdfb37620ab5fab
Sa-Sajjad <sqsa93103@gmail.com>
2024-07-02 07:58:18 +0600

    lavender: Cleanup dead targets Offending entries: TetheringConfigOverlay android.hardware.drm@1.4-service.clearkey android.hardware.graphics.allocator@3.0-impl android.hardware.graphics.allocator@4.0-impl audio_amplifier.sdm660 libbthost_if libldacBT_dec librecovery_updater_lavender
    Signed-off-by: Sa-Sajjad <sqsa93103@gmail.com>

--- device.mk
+++ device.mk
@@ -83,7 +83,6 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@7.1-impl:32 \
     android.hardware.audio.effect@7.0-impl:32 \
     android.hardware.audio.service \
-    audio_amplifier.sdm660 \
     audio.primary.sdm660 \
     audio.r_submix.default \
     audio.usb.default \
@@ -114,14 +113,12 @@ PRODUCT_COPY_FILES += \
 
 # Bluetooth
 PRODUCT_PACKAGES += \
-    libbthost_if \
     audio.bluetooth.default \
     android.hardware.bluetooth@1.0 \
     android.hardware.bluetooth.audio-impl \
     libsndmonitor \
     libhdmiedid \
     libhfp \
-    libldacBT_dec \
     vendor.qti.hardware.bluetooth_audio@2.0.vendor \
     vendor.qti.hardware.btconfigstore@1.0 \
     vendor.qti.hardware.btconfigstore@1.0.vendor
@@ -173,8 +170,6 @@ PRODUCT_PACKAGES += \
     android.frameworks.displayservice@1.0 \
     android.hardware.graphics.allocator@2.0-impl \
     android.hardware.graphics.allocator@2.0-service \
-    android.hardware.graphics.allocator@3.0-impl \
-    android.hardware.graphics.allocator@4.0-impl \
     android.hardware.graphics.mapper@2.0-impl-2.1 \
     android.hardware.graphics.composer@2.1-service \
     android.hardware.memtrack@1.0-impl \
@@ -182,7 +177,6 @@ PRODUCT_PACKAGES += \
 
 # DRM
 PRODUCT_PACKAGES += \
-    android.hardware.drm@1.4-service.clearkey \
     android.hardware.drm@1.4.vendor \
     libhidlmemory.vendor
 
@@ -386,10 +380,6 @@ PRODUCT_PACKAGES += \
     ueventd.qcom.rc \
     fstab.qcom
 
-# Recovery
-PRODUCT_PACKAGES += \
-    librecovery_updater_lavender
-
 # RIL
 PRODUCT_PACKAGES += \
     android.hardware.secure_element@1.0 \
@@ -470,7 +460,6 @@ PRODUCT_PACKAGES += \
     android.hardware.wifi-service \
     hostapd \
     libwifi-hal-qcom \
-    TetheringConfigOverlay \
     libwpa_client \
     WifiOverlay \
     wpa_supplicant \
