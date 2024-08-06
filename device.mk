fd6ea3e5093b9ddabdcafb7e74066da0ca128dd5
GhostMaster69-dev <rathore6375@gmail.com>
2024-08-07 01:42:16 +0530

    lavender: Build missing 32-bit camera lib for 14 QPR3
    Signed-off-by: GhostMaster69-dev <rathore6375@gmail.com>
    Signed-off-by: Sã Śâjjãd <mdsojola12@gmail.com>

--- device.mk
+++ device.mk
@@ -150,6 +150,7 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-service \
     android.hardware.camera.provider@2.5:64 \
     libdng_sdk.vendor \
+    libpng.vendor:32 \
     vendor.qti.hardware.camera.device@1.0:64
 
 # Camera  Shims
